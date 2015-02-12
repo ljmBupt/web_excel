9b9551ae317ef60b67cf7a0edd5a862ea936ad82
刘建明 <liujianming1@liujianmingdeMacBook-Pro.local>
2015-02-12 16:17:30 +0800

    first version

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+
+import os
+import xlrd
 
 from tornado.options import define, options
 
@@ -15,7 +17,8 @@ define("port", default=8000, help="run on the given port", type=int)
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/", SimpleHandler),
+            (r"/", FileHandler),
+            (r"/show", ExcelHandler),
         ]
         settings = dict(
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
@@ -24,17 +27,40 @@ class Application(tornado.web.Application):
         )
         tornado.web.Application.__init__(self, handlers, **settings)
 
-class SimpleHandler(tornado.web.RequestHandler):
-    def read_excel(self, filename="html.xlsx"):
-        excel_path = os.path.join(os.path.dirname(__file__), "html.xlsx")
-        data = xlrd.open_workbook(excel_path)
+class FileHandler(tornado.web.RequestHandler):
+    def gen_filelist(self):
+        excel_path = os.path.join(os.path.dirname(__file__), "excel")
+        excel_files = filter(lambda x: x.split('.')[-1] in ['xls', 'xlsx'], os.listdir(excel_path))
+        return excel_files
 
     def get(self):
+        files = self.gen_filelist()
+        self.render(
+            "files.html",
+            files=files
+        )
+
+class ExcelHandler(tornado.web.RequestHandler):
+    def read_excel(self, filename='html.xls'):
+        excel_path = os.path.join(os.path.dirname(__file__), "excel")
+        file_path = os.path.join(excel_path, filename)
+        try:
+            data = xlrd.open_workbook(file_path)
+        except StandardError, e:
+            print e
+        table = data.sheet_by_index(0)
+        text = []
+        for i in range(table.nrows):
+            text.append(table.row_values(i))
+        return text
+
+    def post(self):
+        filename = self.get_argument('filename')
+        text= self.read_excel(filename)
         self.render(
             "excel.html",
-            title="Excel Page"
-            header="Excel"
-            intro=""
+            filename=filename,
+            text=text
         )
 
 
@@ -45,5 +71,5 @@ def main():
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
